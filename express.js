852846f3b4a4a1e872cc65c8f5b5658ca96edbe8
booldook <booldook@gmail.com>
2019-05-13 14:32:26 +0900

    static

--- express.js
+++ express.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Express!');
+app.use(express.static('public'));
+app.use(express.static('/login'));
+app.get('/book', (req, res) => {
+	var books = [
+		{"title": "홍길동전", floor: "3F"},
+		{"title": "별주부전", floor: "5F"},
+		{"title": "춘향전", floor: "6F"}
+	];
+	var html = '<h2>도서관 비치 도서</h2><ul>';
+	for(var i in books) {
+		html += `<li>${books[i].title} / ${books[i].floor}</li>`;
+	}
+	html += '</ul>';
+	res.send(html);
 });
+/*
+app.get('/', (req, res) => {
+  res.send('Hello Express!!!!!!!!!!!!!');
+});
+*/
 app.listen(3000, () => {
   console.log(`Server running at http://127.0.0.1:3000`);
 });
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const host = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
-	res.stateCode = 200;
+	res.statusCode = 200;
 	res.setHeader('Content-type', 'text/plain');
 	res.end('Hello Node.js');
 });
